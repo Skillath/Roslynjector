8304018a9566938941a4efbc8b0bbde2d227e74b
Xabier Gonzalez Goienetxea <xabi.goienetxea@gmail.com>
2025-12-02 16:53:43 +0100

    Started working on factories

--- Roslyinjector/Roslynjector.Sample/IFoo.cs
+++ Roslyinjector/Roslynjector.Sample/IFoo.cs
@@ -1,21 +1,40 @@
 using System;
 using Microsoft.Extensions.DependencyInjection;
 
-public interface IFoo {}
-public sealed class Foo : IFoo { public Foo(IBar b) {} }
+namespace Roslynjector.Sample;
 
-public interface IBar {}
-public sealed class Bar : IBar {}
+public interface IFoo
+{
+    
+}
+
+public sealed class Foo : IFoo
+{
+    public Foo(IBar b)
+    {
+        
+    }
+}
+
+public interface IBar
+{
+    
+}
+
+public sealed class Bar : IBar
+{
+    
+}
 
 static class Bootstrap
 {
     static void Configure()
     {
         var services = new ServiceCollection();
-        services.AddSingleton<IFoo, Foo>();
-        services.AddTransient<IBar, Bar>();
+        ServiceCollectionServiceExtensions.AddSingleton<IFoo, Foo>(services);
+        ServiceCollectionServiceExtensions.AddTransient<IBar, Bar>(services);
         services.AddScoped<Random>(sp => new Random(7));
-        //services.AddTransient(typeof(Uri), sp => new Uri("https://example.com"));
+        ServiceCollectionServiceExtensions.AddTransient(services, typeof(Uri), sp => new Uri("https://example.com"));
         
     }
 }
